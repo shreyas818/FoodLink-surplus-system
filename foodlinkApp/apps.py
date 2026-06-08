42bef752a1018ae0d2917b8439c08784c6750400
ShreyasR <shre0818@gmail.com>
2026-06-08 11:18:16 +0530

    Auto-create shreyas superuser on startup if not present

--- foodlinkApp/apps.py
+++ foodlinkApp/apps.py
@@ -4,3 +4,18 @@ from django.apps import AppConfig
 class FoodlinkappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'foodlinkApp'
+
+    def ready(self):
+        import os
+        from django.db import connection
+        
+        # Ensure database tables exist before querying
+        if 'foodlinkApp_customuser' in connection.introspection.table_names():
+            from .models import CustomUser
+            if not CustomUser.objects.filter(username='shreyas').exists():
+                password = os.environ.get('SUPERUSER_PASSWORD', 'NewPassword123')
+                CustomUser.objects.create_superuser(
+                    username='shreyas',
+                    email='admin@foodlink.org',
+                    password=password
+                )
